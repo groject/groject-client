398f2e7be229256520877428fbc562d7ff412288
jsm8109jsm <jsm8109jsm@gmail.com>
2022-10-06 10:42:09 +0900

    FEAT :: Modal 완성

--- src/components/select.js
+++ src/components/select.js
@@ -3,7 +3,7 @@ import { jsx, css } from '@emotion/react'
 import React, { useState } from 'react';
 
 
-function Select({ display, label, width, height, id, title, margin, cssProp, event }) {
+function Select({ display, label, width, height, id, title, margin, cssProp, event, fontSize, divStyle }) {
 
   const [selected, setSelected] = useState('');
 
@@ -13,27 +13,29 @@ function Select({ display, label, width, height, id, title, margin, cssProp, eve
         font-size: 1.56rem;
         box-sizing: border-box;
         margin-bottom: 16px;
+        ${divStyle}
       `}>
-        <span css={
-          css`
+      <span css={
+        css`
             display: ${display};
             width: 150px;
             margin-right: 44px;
             text-align: right;
           `
-        }>
-          {label}
-        </span>
-        <button id={id} css={
-          css`
+      }>
+        {label}
+      </span>
+      <button id={id} css={
+        css`
             width: ${width};
             height: ${height};
             background: #FBFBFB;
+            cursor: pointer;
             border: 0;
             box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.25);
             border-radius: 10px;
             font-family: 'Inter', BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            font-size: 1.56rem;
+            font-size: ${fontSize};
             margin-left: ${margin};
             color: #6C6C6C;
             ${cssProp};
@@ -41,13 +43,13 @@ function Select({ display, label, width, height, id, title, margin, cssProp, eve
               width: 15.6vw;
             }
           `}
-          onClick={event}
-        >
-          {title}
-        </button>
+        onClick={event}
+      >
+        {title}
+      </button>
     </div>
   )
-  
+
 }
 
 export default Select;
